fb524cac1c365ad955a403eebc159513b0801a46
koyl306 <e.grigorenko@istu.edu.ua>
2026-05-17 20:16:41 +0300

    7

---
+++ 7/Experiment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pract7.database
+{
+    public class Experiment
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Date { get; set; }
+        public string UsedMaterial { get; set; }
+        public string Result { get; set; }
+    }
+}
